441946cc97681b5f0101175c850fe1cdc8b74eda
Rosario Calisaya <misa26amane@gmail.com>
2024-12-16 19:03:34 -0400

    arreglando colors en reportes director

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="nav-links">
       <!-- Si el usuario está en EncuestaEstudiante o GestionDirectores, solo mostrar "Volver" y "Soporte" -->
-      <template v-if="isEncuestaEstudiante || isGestionDirectores || isEnviarEncuesta || isListadoEstudiantes || isResumePage || isNoticiaForm || isFormularioSoporte || isContactoAdmin || isEstudiantesRegistrados || isFormularioPlazos || isPorcentajeIncompleto || isGestionEncuesta || isSoporteDirector || isVerPreguntas || isEstadoEstudiante || isDashboard ||isReporteDashboard || isSubirCertificado
+      <template v-if="isEncuestaEstudiante || isGestionDirectores || isEnviarEncuesta || isListadoEstudiantes || isResumePage || isNoticiaForm || isFormularioSoporte || isContactoAdmin || isEstudiantesRegistrados || isFormularioPlazos || isPorcentajeIncompleto || isGestionEncuesta || isSoporteDirector || isVerPreguntas || isEstadoEstudiante || isDashboard ||isReporteDashboard || isSubirCertificado ||isHistorialReportes
       ">
         <!-- Botón "Volver" con icono -->
         <button @click="goToPreviousPage" class="icon-button volver-icon" title="Volver">
@@ -166,6 +166,9 @@ export default {
     },
     isGestionDirectores() {
       return this.$route.path === '/gestion-directores';
+    },
+    isHistorialReportes() {
+      return this.$route.path === '/historial-reportes';
     },
     isSubirCertificado() {
       return this.$route.path === '/subir-certificado';
